3122d6514f6ca522f23123f9dc1cc4a9fec642a7
Simon Zheng <simz97@outlook.com>
2018-01-30 23:01:10 -0500

    HW1 Q1a)iii) last attempt?

--- hw1/q1.py
+++ hw1/q1.py
@@ -4,8 +4,11 @@
 INIT = ( 1 , 4 , 2 ,
          5 , 3 , 0 )
 
-GOAL = ( 0 , 1 , 2 ,
-         5 , 4 , 3 )
+# GOAL = ( 0 , 1 , 2 ,
+#          5 , 4 , 3 )
+
+GOAL = ( 1 , 0 , 4 ,
+         5 , 3 , 2 )
 
 def print_board(board):
     print("""
@@ -16,35 +19,35 @@ _____________
 """ % (board[0], board[1], board[2], board[3], board[4], board[5]))
 
 def move_up(board, idx):
-    if idx < 3 or idx > 5:
-        return ()
+    # if idx < 3 or idx > 5:
+    #     return ()
     board = list(board)
-    board[idx-3] = board[idx]
-    board[idx] = 0
+    board[idx] = board[idx+3]
+    board[idx+3] = 0
     return tuple(board)
 
 def move_right(board, idx):
-    if idx == 2 or idx == 5:
-        return ()
+    # if idx == 2 or idx == 5:
+    #     return ()
     board = list(board)
-    board[idx+1] = board[idx]
-    board[idx] = 0
+    board[idx] = board[idx-1]
+    board[idx-1] = 0
     return tuple(board)
 
 def move_down(board, idx):
-    if idx < 0 or idx > 2:
-        return ()
+    # if idx < 0 or idx > 2:
+    #     return ()
     board = list(board)
-    board[idx+3] = board[idx]
-    board[idx] = 0
+    board[idx] = board[idx-3]
+    board[idx-3] = 0
     return tuple(board)
 
 def move_left(board, idx):
-    if idx == 0 or idx == 3:
-        return ()
+    # if idx == 0 or idx == 3:
+    #     return ()
     board = list(board)
-    board[idx-1] = board[idx]
-    board[idx] = 0
+    board[idx] = board[idx+1]
+    board[idx+1] = 0
     return tuple(board)
 
 OPS = {"up":move_up, "right":move_right, "down":move_down, "left":move_left}
@@ -52,19 +55,14 @@ VALID_OPS = {0:(move_up, move_left), 1:(move_up, move_left, move_right), 2:(move
              3:(move_down, move_left), 4:(move_down, move_left, move_right), 5:(move_down, move_right)}
 
 def create_children(board, idx):
-    # ops = []
     # idx is location of empty square
-    for i in (idx-3, idx+1, idx+3, idx-1):
-        if idx < 3 or idx > 5:
-            continue
-        if idx == 2 or idx == 5:
-            return ()
-        if idx < 0 or idx > 2:
-            return ()
-        if idx == 0 or idx == 3:
-            return ()
+    children = []
     global VALID_OPS
-    return VALID_OPS[idx]
+    children = [action(board, idx) for action in VALID_OPS[idx]]
+    print children
+    children = sorted(children, cmp=lambda child1, child2: child2[idx] - child1[idx])
+    print children
+    return children
 
 def dfs(board):
     stack = []
@@ -77,22 +75,44 @@ def dfs(board):
     while stack:
         crt_brd = stack.pop(-1)
         visited.add(crt_brd)
-        if crt_brd == GOAL:
-            return path
-        if crt_brd not in visited:
-            visited.add(crt_brd)
-        for i in [i if i > 0 and i < 5 else 6 for i in (idx-3, idx+1, idx+3, idx-1)]:
-            for op in OPS.iteritems():
-                nxt_brd = op[1](crt_brd, i)
-                idx = i
-                stack.append(nxt_brd)
-                path.append(op[0])
+        if (crt_brd == GOAL) or path:
+            node = stack.pop(-1)
+            if node in visited:
+                path.append(node)
+            continue
+        nxt_brds = []
+        for child in create_children(board, idx):
+            if child not in visited:
+                nxt_brds.append(child)
+        if not nxt_brds:
+            stack.pop(-1)
+        else:
+            stack += nxt_brds
     return path
+
+def rec_dfs(board, idx, visited, path, goal, limit):
+    visited.add(board)
+    if board == GOAL:
+        return path
+    elif limit == 0:
+        return []
+    else:
+        for child in create_children(board, idx):
+            path.append(child)
+            result_path = rec_dfs(child, idx, visited, path, goal, limit-1)
+            if result_path:
+                return result_path
+            else:
+                path.pop(-1)
+    return []
 
 def main():
     global INIT
-    path = dfs(INIT)
-    print_board(path)
+    global GOAL
+    # path = dfs(INIT)
+    path = rec_dfs(INIT, 5, set(), [], GOAL, 18)
+    print_board(INIT)
+    print(path)
     return 0
 
 if __name__ == '__main__':
