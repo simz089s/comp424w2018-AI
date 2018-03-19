55f7dd0950fd04eadc026fb6ca706cf8c1fd4f5c
Simon Zheng <simz97@outlook.com>
2018-03-19 13:49:34 -0400

    HW3 Add calc.py

---
+++ hw3/calc.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding : utf-8 -*-
+#
+# calc.py
+from __future__ import print_function
+import os, sys
+import numpy as np
+import scipy as sp
+
+
+def main(args):
+    print(args)
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
