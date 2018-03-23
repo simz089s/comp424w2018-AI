d4822492c1df233bbe52d5e380c60e24b647cb24
Simon Zheng <simz97@outlook.com>
2018-03-23 15:26:37 -0400

    HW3 Day 2

--- hw3/calc.py
+++ hw3/calc.py
@@ -13,23 +13,15 @@ PROBS = {
     b : 0.2, r : 0.15, "t|r,b,a" : 0.95, "t|r,-b,a" : 0.92, "t|r,b,-a" : 0.90,
     "t|r,-b,-a" : 0.85, "t|-r,b,a" : 0.35, "t|-r,b,-a" : 0.4, "t|-r,-b,a" : 0.6,
     "t|-r,-b,-a" : 0.05, "s|a" : 0.8, "s|-a" : 0.2, "a|b": 0.6, "a|-b" : 0.4,
+    '-b' : 1-0.2, a : 0.44, '-a' : 1-0.44, s : 0.464, '-s' : 1-0.464,
+    't,s' : 0.232048
 }
 
 def P(x):
-    try:
-        return PROBS[x]
-    except KeyError:
-        return P2(x)
-
-PROBS2 = {
-    "s,r" : P(r) * P("s|a") * P("a|b")
-}
-
-def P2(x):
-    return PROBS2[x]
+    return PROBS[x]
 
 def main(args):
-    print( "P(s,r) = ", P("s,r") )
+    print( P('t,s') / P(s) )
     return 0
 
 if __name__ == "__main__":
